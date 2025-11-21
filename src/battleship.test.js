5d60d4d056d9dc8f37e8db0e839d340bfdbfe18c
Ray <166890725+Hashcat07@users.noreply.github.com>
2025-11-21 21:01:44 +0530

    feat: add Game Board and tests for Game Board
    - Tests only place() and receiveHits() methods

--- src/battleship.test.js
+++ src/battleship.test.js
@@ -1,8 +1,9 @@
 import { Ship } from "./ship";
+import { GameBoard } from "./gameBoard";
 
 describe('ship test',()=>{
     let ship
-    beforeEach(()=>{ship=new Ship(3,"x")})
+    beforeEach(()=>{ship=new Ship(3)})
     test ('ship hit test',()=>{    
         ship.hit()
         expect(ship.damage).toBe(1)
@@ -14,5 +15,39 @@ describe('ship test',()=>{
         ship.hit()
         ship.hit()
         expect(ship.sunk).toBe(true)
+    })
+})
+
+describe('Gameboard Place test',()=>{
+    let gameBoard
+    beforeAll(()=>{gameBoard=new GameBoard('test')})
+    test('place ships',()=>{
+        expect(gameBoard.place('ca',"x",2,0)).toBeTruthy()
+    })
+    test('place ships(false)',()=>{
+        expect(gameBoard.place('ca','y',2,4)).toBeFalsy()
+    })
+   
+})
+
+describe('Gameboad Recieve Hit test',()=>{
+    let gameBoard
+    beforeAll(()=>{gameBoard=new GameBoard('test')
+        gameBoard.place('ca',"x",2,0)
+    })
+     test('Attck ships',()=>{
+        
+        expect(gameBoard.recieveAttack(2,0)).toBe('Hit')
+    })
+    test('Attack ships (false)',()=>{
+        expect(gameBoard.recieveAttack(3,0)).toBe('Miss')
+    })
+    test('Attack Ships (outOfBound)',()=>{
+        expect(gameBoard.recieveAttack(11,0)).toBe('Out of Board')
+        expect(gameBoard.recieveAttack(0,11)).toBe('Out of Board')
+    })
+    test('Attack already attacked target',()=>{
+        gameBoard.recieveAttack(2,0)
+        expect(gameBoard.recieveAttack(2,0)).toBe('already attacked')
     })
 })
---
+++ src/gameBoard.js
@@ -0,0 +1,67 @@
+import { Ship } from "./ship"
+export class GameBoard{
+    constructor(player){
+        this.player=player
+        this.board= Array.from({ length: 10 }, () => Array(10).fill(null));
+        this.shipUsed=[]
+        this.ships=[]
+        this.missedShots=[]
+    }
+    shipType(type){
+        if(type==='d')return 2
+        if(type==='s')return 3
+        if(type==='cr')return 3
+        if(type==='b')return 4
+        if(type==='ca')return 5
+    }
+    place(type,axis,xAxis,yAxis){
+        let shipLength=this.shipType(type);
+        if(!this.shipUsed.includes(type))
+    {
+        const ship=new Ship(shipLength)
+        this.shipUsed.push(type)
+        this.ships.push(ship)
+        
+       if (axis==='x') { 
+       
+        for(let i=0;i<shipLength;i++)
+         {
+           
+            this.board[xAxis][yAxis+i]=ship
+
+         }
+       }
+       if(axis==='y'){
+       for(let i=0;i<shipLength;i++)
+         {
+            
+            this.board[xAxis+i][yAxis]=ship
+         }}
+         return true
+    }
+    else
+    return false
+
+    }
+    recieveAttack(xAxis,yAxis){
+        
+        if(xAxis>9||yAxis>9||xAxis<0||yAxis<0)
+            return 'Out of Board'
+    
+        let boardPosition=this.board[xAxis][yAxis]
+        if(this.missedShots.some(([x,y])=>x==xAxis&&y==yAxis))
+            return 'already attacked'
+        if(boardPosition instanceof Ship){
+            boardPosition.hit()
+            this.board[xAxis][yAxis]='hit'
+            return 'Hit'
+        }
+        else{
+            boardPosition=1
+            this.missedShots.push([xAxis,yAxis])
+            return 'Miss'
+        }
+        
+
+    }
+}
