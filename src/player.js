146997f8013b62460c2961a08a0cfed3cab4b354
Ray <166890725+Hashcat07@users.noreply.github.com>
2025-11-22 22:41:26 +0530

    feat(filename): add makeMove and fix logic in player class
    - Fixes the Player creating error where all the players are not bots
    - Add method to help bot make a move

--- src/player.js
+++ src/player.js
@@ -2,7 +2,16 @@ import { GameBoard } from "./gameBoard";
 
 export class Player{
     constructor(isBot= false){
-        if(isBot===true) this.gameBoard=new GameBoard('Bot')
+        this.bot=isBot
+        if(bot===true) this.gameBoard=new GameBoard('Bot')
+        else
         this.gameBoard = new GameBoard('Player')
+    }
+    makeMove(){
+        if(!this.bot)return 
+
+        let x = Math.floor(Math.random() * 10);
+        let y = Math.floor(Math.random() * 10);
+        return [x,y]
     }
 }
